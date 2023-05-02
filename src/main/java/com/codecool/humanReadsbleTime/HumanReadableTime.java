59cac7375e7552428709ea0eea684b3df38c3b3a
Philippovitsch <philipp@stadter.at>
2023-05-02 11:35:56 +0200

    Add new kata: Human Readable Time

---
+++ src/main/java/com/codecool/humanReadsbleTime/HumanReadableTime.java
@@ -0,0 +1,21 @@
+package com.codecool.humanReadsbleTime;
+
+public class HumanReadableTime {
+
+    private final int HOURS = 3600;
+    private final int MINUTES = 60;
+
+    public String makeReadable(int seconds) {
+        int convertedHours = Math.floorDiv(seconds, HOURS);
+        int convertedMinutes = Math.floorDiv(seconds % HOURS, MINUTES);
+        int convertedSeconds = (seconds % HOURS) % MINUTES;
+        return String.format(
+                "%02d:%02d:%02d",
+                convertedHours,
+                convertedMinutes,
+                convertedSeconds
+
+        );
+    }
+
+}
---
+++ src/test/java/com/codecool/humanReadableTime/HumanReadableTimeTest.java
@@ -0,0 +1,27 @@
+package com.codecool.humanReadableTime;
+
+import com.codecool.humanReadsbleTime.HumanReadableTime;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HumanReadableTimeTest {
+
+    HumanReadableTime time = new HumanReadableTime();
+
+    @ParameterizedTest
+    @CsvSource({
+            "00:00:00, 0",
+            "00:00:01, 1",
+            "00:00:05, 5",
+            "00:01:00, 60",
+            "23:59:59, 86399",
+            "99:59:58, 359998",
+            "99:59:59, 359999"
+    })
+    public void makeReadableTest(String result, int seconds) {
+        assertEquals(result, time.makeReadable(seconds));
+    }
+
+}
