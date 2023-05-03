375f8af49f7aa9c4901dbc13d873ba341f29bee0
Philippovitsch <philipp@stadter.at>
2023-05-03 10:37:36 +0200

    Add new kata: Spaghetti Code #1

---
+++ src/main/java/com/codecool/spaghettiCode/SpaghettiCode1.java
@@ -0,0 +1,34 @@
+package com.codecool.spaghettiCode;
+
+import java.util.Arrays;
+
+public class SpaghettiCode1 {
+
+    private final int letterA = 65;
+    private final int letterZ = 90;
+
+    public String getLongestPiece(String[][] spaghettiPlate) {
+        String longestSpaghetti = "";
+        long numberOfLongestPieces = 0;
+
+        for(int character = letterA; character <= letterZ; character++) {
+            long spaghettiCounter = 0;
+            for(String[] spaghettiRow : spaghettiPlate) {
+                spaghettiCounter += countSpaghettiPieces(spaghettiRow, character);
+            }
+            if (spaghettiCounter > numberOfLongestPieces) {
+                longestSpaghetti = String.valueOf((char) character);
+                numberOfLongestPieces = spaghettiCounter;
+            }
+        }
+
+        return longestSpaghetti;
+    }
+
+    private long countSpaghettiPieces(String[] spaghettiRow, int character) {
+        return Arrays.stream(spaghettiRow)
+                .filter(spaghettiPiece -> spaghettiPiece.charAt(0) == character)
+                .count();
+    }
+
+}
