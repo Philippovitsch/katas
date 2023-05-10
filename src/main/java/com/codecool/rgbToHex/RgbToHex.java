580ca918945e05e339b3b7d7513b44a583389e49
Philippovitsch <philipp@stadter.at>
2023-05-10 22:16:58 +0200

    Add new kata: RGB To Hex Conversion

---
+++ src/main/java/com/codecool/rgbToHex/RgbToHex.java
@@ -0,0 +1,35 @@
+package com.codecool.rgbToHex;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RgbToHex {
+
+    // Source: https://www.codewars.com/kata/513e08acc600c94f01000001
+
+    // First variant
+    public String convert1(int r, int g, int b) {
+        return getHex(r) + getHex(g) + getHex(b);
+    }
+
+    private String getHex(int colorValue) {
+        if (colorValue < 0) {
+            colorValue = 0;
+        }
+        if (colorValue > 255) {
+            colorValue = 255;
+        }
+
+        return String.format("%02X", colorValue);
+    }
+
+    // Second variant with stream
+    public String convert2(int r, int g, int b) {
+        return Stream.of(r, g, b)
+                .map(value -> value < 0 ? 0 : value)
+                .map(value -> value > 255 ? 255 : value)
+                .map(value -> String.format("%02X", value))
+                .collect(Collectors.joining());
+    }
+
+}
---
+++ src/test/java/com/codecool/rgbToHex/RgbToHexTest.java
@@ -0,0 +1,25 @@
+package com.codecool.rgbToHex;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RgbToHexTest {
+
+    RgbToHex rgbToHex = new RgbToHex();
+
+    @ParameterizedTest
+    @CsvSource({
+            "000000, 0, 0, 0",
+            "010203, 1, 2, 3",
+            "FFFFFF, 255, 255, 255",
+            "FEFDFC, 254, 253, 252",
+            "00FF7D, -20, 275, 125"
+    })
+    public void sampleTests(String result, int r, int g, int b) {
+        assertEquals(result, rgbToHex.convert1(r, g, b));
+        assertEquals(result, rgbToHex.convert2(r, g, b));
+    }
+
+}
